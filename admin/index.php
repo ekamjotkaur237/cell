f777e4ef4c6ac3c386b7a0eb5d3c9d3577c475a1
ekamjotkaur237 <ekamjotkaur237@gmail.com>
2025-10-08 13:32:03 -0400

    Redirection
    change

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ require_once('./lib/header.php');
             <div class="card-body">
                 <h5 class="card-title">Create a new Club</h5>
                 <p class="card-text">Add, edit, or remove clubs from the election.</p>
-                <a href="#" class="btn btn-light">Go to Clubs</a>
+                <a href="club.php" class="btn btn-light">Go to Clubs</a>
             </div>
         </div>
     </div>
@@ -23,7 +23,7 @@ require_once('./lib/header.php');
             <div class="card-body">
                 <h5 class="card-title">Manage Candidates</h5>
                 <p class="card-text">Add, edit, or remove candidates from the election.</p>
-                <a href="#" class="btn btn-light">Go to Candidates</a>
+                <a href="candidates.php" class="btn btn-light">Go to Candidates</a>
             </div>
         </div>
     </div>
@@ -33,21 +33,10 @@ require_once('./lib/header.php');
             <div class="card-body">
                 <h5 class="card-title">Manage Voters</h5>
                 <p class="card-text">View and manage the list of registered voters.</p>
-                <a href="#" class="btn btn-light">Go to Voters</a>
+                <a href="voters.php" class="btn btn-light">Go to Voters</a>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="card text-white bg-info mb-3">
-            <div class="card-header">Results</div>
-            <div class="card-body">
-                <h5 class="card-title">View Results</h5>
-                <p class="card-text">See the real-time election results and statistics.</p>
-                <a href="#" class="btn btn-light">Go to Results</a>
-            </div>
-        </div>
-    </div>
-</div>
 
 <script>
     document.getElementById('dashboardtab').classList.add('active');
